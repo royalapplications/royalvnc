73eeb7278276d96e464af18da83d32f2b32ee0b0
Felix Deimel <felix@lemonmojo.com>
2024-09-30 12:22:55 +0200

    Remove zlib swift package

--- Package.swift
+++ Package.swift
@@ -22,26 +22,31 @@ let package = Package(
     ],
 
     dependencies: [
-        .package(url:"https://github.com/fourplusone/swift-package-zlib", branch: "master"),
         .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" ..< "4.0.0")
     ],
     
     targets: [
         .target(
             name: "RoyalVNCKit",
+            
             dependencies: [
                 "d3des",
                 "libtommath",
+                "Z",
                 
-                .product(name: "Z", package:"swift-package-zlib"),
                 .product(name: "Crypto", package: "swift-crypto")
             ],
+            
             swiftSettings: [
                 .swiftLanguageMode(.v5)
             ]
         ),
         
         .target(name: "d3des"),
-        .target(name: "libtommath")
+        .target(name: "libtommath"),
+        
+        .target(name: "Z", linkerSettings: [
+            .linkedLibrary("z")
+        ])
     ]
 )
