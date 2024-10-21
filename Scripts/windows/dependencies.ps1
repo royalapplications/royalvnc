2047fd0093304fafe604967f0606f988a3dc7a24
nil4 <nil4@users.noreply.github.com>
2024-10-21 15:13:53 +0200

    [windows] add `zlib-ng`
    Build with `ZLIB_COMPAT=yes` and deploy to `Sources/Z-win`
    
    `zlib-ng` builds on Windows without baroque workarounds
    - ref. https://github.com/dotnet/runtime/issues/101465
    - ref. https://github.com/dotnet/runtime/issues/105373

--- Scripts/windows/dependencies.ps1
+++ Scripts/windows/dependencies.ps1
@@ -14,25 +14,35 @@ exec { swift --version }
 
 $BIN_DIR = Join-Path $ROOT_PATH "bin" -Resolve
 
-function download_and_extract([string]$repo, [string]$version, [string]$file) {
-    [string]$filename = "${file}-${version}.zip"
+function download_and_extract([string]$releaseFile, [string]$localName) {
+    [string]$filename = "${localName}.zip"
     if (-not (file_exists "${BIN_DIR}\${filename}")) {
-        exec { curl.exe -sSL -o "${BIN_DIR}\${filename}" "https://github.com/libtom/${repo}/releases/download/v${version}/${filename}" }
+        exec { curl.exe -sSL -o "${BIN_DIR}\${filename}" "https://github.com/${releaseFile}" }
     }
+    if (-not (dir_exists "${BIN_DIR}\${localName}")) {
+        remove_dir "${BIN_DIR}\_tmp"
+        create_dir "${BIN_DIR}\_tmp"
+        Expand-Archive "${BIN_DIR}\${filename}" "${BIN_DIR}\_tmp"
 
-    Expand-Archive "${BIN_DIR}\${filename}" "${BIN_DIR}"
-    return "${BIN_DIR}\${repo}-${version}"
+        if (dir_exists "${BIN_DIR}\_tmp\${localName}") {
+            Move-Item "${BIN_DIR}\_tmp\${localName}" "${BIN_DIR}\${localName}"
+        } else {
+            Move-Item "${BIN_DIR}\_tmp" "${BIN_DIR}\${localName}"
+        }
+    }
+    return Join-Path $BIN_DIR $localName -Resolve
 }
 
-$mathDir  = download_and_extract 'libtommath'  '1.3.0'  'ltm'
-$cryptDir = download_and_extract 'libtomcrypt' '1.18.2' 'crypt'
+$mathDir  = download_and_extract 'libtom/libtommath/releases/download/v1.3.0/ltm-1.3.0.zip'      'libtommath-1.3.0'
+$cryptDir = download_and_extract 'libtom/libtomcrypt/releases/download/v1.18.2/crypt-1.18.2.zip' 'libtomcrypt-1.18.2'
+$zlibDir  = download_and_extract 'zlib-ng/zlib-ng/archive/refs/tags/2.2.2.zip'                   'zlib-ng-2.2.2'
 
-function nmake_build([string]$dir, [string[]]$nmakeArgs) {
+function nmake_build([string]$dir, [string]$makefile, [string[]]$nmakeArgs) {
     Write-Host "Building ${dir}" -ForegroundColor Cyan
 
     Push-Location $dir
     try {
-        exec { nmake.exe /nologo -f "makefile.msvc" $nmakeArgs }
+        exec { nmake.exe /nologo -f $makefile $nmakeArgs }
     } finally {
         Pop-Location
     }
@@ -46,10 +56,15 @@ if ($HOST_MSVC_ARCH -eq "arm64") {
     $endian_flags = '/DENDIAN_LITTLE /DENDIAN_64BITWORD /DLTC_FAST'
 }
 
-nmake_build $mathDir  @()
-nmake_build $cryptDir @(
+nmake_build $mathDir  "makefile.msvc" @()
+nmake_build $cryptDir "makefile.msvc" @(
     "CFLAGS=/DUSE_LTM /DLTM_DESC ${endian_flags} /I${mathDir}"
     "EXTRALIBS=${mathDir}\tommath.lib"
+)
+
+$zlibMakefile = if ($HOST_MSVC_ARCH -eq "arm64") { 'win32/Makefile.a64' } else { 'win32/Makefile.msc' }
+nmake_build $zlibDir $zlibMakefile @(
+    "ZLIB_COMPAT=yes"
 )
 
 function make_bundle([string]$targetDir, [string]$headers, [string]$libs) {
@@ -63,5 +78,6 @@ function make_bundle([string]$targetDir, [string]$headers, [string]$libs) {
     Copy-Item "${libs}"    "${targetDir}"         -Force
 }
 
-make_bundle "${ROOT_PATH}\Sources\libtommath-win"  "${mathDir}\*.h"              "${mathDir}\*.lib"
-make_bundle "${ROOT_PATH}\Sources\libtomcrypt-win" "${cryptDir}\src\headers\*.h" "${cryptDir}\*.lib"
+make_bundle "${ROOT_PATH}\Sources\libtommath-win"  "${mathDir}\*.h"              "${mathDir}\tommath.lib"
+make_bundle "${ROOT_PATH}\Sources\libtomcrypt-win" "${cryptDir}\src\headers\*.h" "${cryptDir}\tomcrypt.lib"
+make_bundle "${ROOT_PATH}\Sources\Z-win"           "${zlibDir}\*.h"              "${zlibDir}\zlib.lib"
