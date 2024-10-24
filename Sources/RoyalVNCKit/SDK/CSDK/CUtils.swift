db699b363506d1ada2ae46a28baa29b3694f7381
nil4 <nil4@users.noreply.github.com>
2024-10-24 19:58:43 +0200

    [windows] avoid `strdup` deprecation warning

--- Sources/RoyalVNCKit/SDK/CSDK/CUtils.swift
+++ Sources/RoyalVNCKit/SDK/CSDK/CUtils.swift
@@ -14,6 +14,11 @@ import Glibc
 
 extension String {
     func duplicateCString() -> UnsafeMutablePointer<CChar>? {
+#if canImport(WinSDK)
+        // avoid: warning: 'strdup' is deprecated: The POSIX name for this item is deprecated. Instead, use the ISO C and C++ conformant name: _strdup
+        return _strdup(self)
+#else
         return strdup(self)
+#endif
     }
 }
