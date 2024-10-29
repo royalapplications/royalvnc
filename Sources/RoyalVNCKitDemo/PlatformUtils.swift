a0d56ce6a7db9a0a52ff5f655dfde3f19ade1329
Felix Deimel <felix@lemonmojo.com>
2024-10-29 16:52:16 +0100

    Implement readPassword for Windows in Swift Demo

--- Sources/RoyalVNCKitDemo/PlatformUtils.swift
+++ Sources/RoyalVNCKitDemo/PlatformUtils.swift
@@ -50,12 +50,111 @@ func readPassword(prompt: String) -> String? {
     
     return password
 #else
-    // TODO: Implement password input for Windows
+    let len = 4096
 
-    print(prompt, terminator: "")
-    
-    let password = readLine(strippingNewline: true)
+    /* Resources that will be cleaned up */
+    var pwlen: Int32 = 0
+    var orig: DWORD = 0
+    var wbuf: UnsafeMutableRawPointer?
+    var wbuf_len: SIZE_T = 0
+    var hi = INVALID_HANDLE_VALUE
+    var ho = INVALID_HANDLE_VALUE
 
-    return password
+    func cleanUp() {
+        if let wbuf {
+            RtlSecureZeroMemory(wbuf, .init(wbuf_len))
+            HeapFree(GetProcessHeap(), 0, wbuf)
+        }
+
+        /* Exploit that operations on INVALID_HANDLE_VALUE are no-ops */
+        WriteConsoleA(ho, "\n", 1, nil, nil)
+        SetConsoleMode(hi, orig)
+        CloseHandle(ho)
+        CloseHandle(hi)
+    }
+
+    /* Set up input console handle */
+    let access = DWORD(GENERIC_READ) | DWORD(GENERIC_WRITE)
+    hi = CreateFileA("CONIN$", access, 0, nil, .init(OPEN_EXISTING), 0, nil)
+
+    guard GetConsoleMode(hi, &orig) else {
+        cleanUp()
+
+        return nil
+    }
+
+    var mode = orig
+    mode |= .init(ENABLE_PROCESSED_INPUT)
+    mode |= .init(ENABLE_LINE_INPUT)
+    mode &= ~(.init(ENABLE_ECHO_INPUT))
+
+    guard SetConsoleMode(hi, mode) else {
+        cleanUp()
+
+        return nil
+    }
+
+    /* Set up output console handle */
+    ho = CreateFileA("CONOUT$", .init(GENERIC_WRITE), 0, nil, .init(OPEN_EXISTING), 0, nil)
+
+    guard WriteConsoleA(ho, prompt, .init(strlen(prompt)), nil, nil) else {
+        cleanUp()
+
+        return nil
+    }
+
+    /* Allocate a wide character buffer the size of the output */
+    wbuf_len = (SIZE_T(len) - 1 + 2) * SIZE_T(MemoryLayout<WCHAR>.stride)
+    wbuf = HeapAlloc(GetProcessHeap(), 0, wbuf_len)
+
+    guard let wbuf else {
+        cleanUp()
+
+        return nil
+    }
+
+    /* Read and convert to UTF-8 */
+    var nread: DWORD = 0
+
+    guard ReadConsoleW(hi, wbuf, DWORD(len) - 1 + 2, &nread, nil) else {
+        cleanUp()
+
+        return nil
+    }
+
+    guard nread >= 2 else {
+        cleanUp()
+
+        return nil
+    }
+
+    // TODO: Do this to the passwordBuf string
+    let wbufAsWchar = UnsafeMutablePointer<WCHAR>(OpaquePointer(wbuf))
+
+    if wbufAsWchar[Int(nread)-2] != UInt16("\r".utf16.first!) ||
+       wbufAsWchar[Int(nread)-1] != UInt16("\n".utf16.first!) {
+        cleanUp()
+
+        return nil
+    }
+
+    wbufAsWchar[Int(nread)-2] = 0 // truncate "\r\n"
+
+    guard let buf = malloc(MemoryLayout<CChar>.stride * len) else {
+        cleanUp()
+
+        return nil
+    }
+
+    defer {
+        free(buf)
+    }
+
+    pwlen = WideCharToMultiByte(.init(CP_UTF8), 0, .init(OpaquePointer(wbuf)), -1, buf, .init(len), nil, nil);
+
+    let bufAsCCharPtr = UnsafeMutablePointer<CChar>(OpaquePointer(buf))
+    let passwordBuf = String(cString: bufAsCCharPtr)
+
+    return passwordBuf
 #endif
 }
