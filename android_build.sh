573c1d2f8af1e0811b34c35e14a51a0c0fe38df9
nil4 <nil4@users.noreply.github.com>
2025-10-24 12:45:12 +0200

    [android] drop `x86_64` jniLibs

--- android_build.sh
+++ android_build.sh
@@ -8,15 +8,12 @@ SWIFT_ANDROID_ROOT="${HOME}/Library/org.swift.swiftpm/swift-sdks/swift-6.2-RELEA
 
 SWIFT_ANDROID_NDK_BASE="${SWIFT_ANDROID_ROOT}/ndk-sysroot/usr/lib"
 SWIFT_ANDROID_NDK_ARM64="${SWIFT_ANDROID_NDK_BASE}/aarch64-linux-android"
-SWIFT_ANDROID_NDK_X86_64="${SWIFT_ANDROID_NDK_BASE}/x86_64-linux-android"
 
 SWIFT_ANDROID_SDK_BASE="${SWIFT_ANDROID_ROOT}/swift-resources/usr/lib"
 SWIFT_ANDROID_SDK_ARM64="${SWIFT_ANDROID_SDK_BASE}/swift-aarch64/android"
-SWIFT_ANDROID_SDK_X86_64="${SWIFT_ANDROID_SDK_BASE}/swift-x86_64/android"
 
 KOTLIN_TARGET_JNI_BASE="Bindings/kotlin/RoyalVNCAndroidTest/app/src/main/jniLibs"
 KOTLIN_TARGET_JNI_ARM64="${KOTLIN_TARGET_JNI_BASE}/arm64-v8a"
-KOTLIN_TARGET_JNI_X86_64="${KOTLIN_TARGET_JNI_BASE}/x86_64"
 
 declare -a SWIFT_NDK_LIBS=(
 	"${ANDROID_API_LEVEL}/libz.so"
@@ -44,16 +41,10 @@ skip android build \
 	--configuration release \
 	--no-bridge \
 	--android-api-level ${ANDROID_API_LEVEL} \
-	--arch aarch64 \
-	--arch x86_64
+	--arch aarch64
 
 echo "Cleaning Kotlin JNI ARM64 libraries"
 pushd "${KOTLIN_TARGET_JNI_ARM64}"
-rm -f *.so
-popd
-
-echo "Cleaning Kotlin JNI x86_64 libraries"
-pushd "${KOTLIN_TARGET_JNI_X86_64}"
 rm -f *.so
 popd
 
@@ -61,11 +52,6 @@ echo "Copying RoyalVNC ARM64 library"
 cp -f \
 	.build/aarch64-unknown-linux-android${ANDROID_API_LEVEL}/release/libRoyalVNCKit.so \
 	"${KOTLIN_TARGET_JNI_ARM64}/"
-
-echo "Copying RoyalVNC x86_64 library"
-cp -f \
-	.build/x86_64-unknown-linux-android${ANDROID_API_LEVEL}/release/libRoyalVNCKit.so \
-	"${KOTLIN_TARGET_JNI_X86_64}/"
 
 # Swift Libs
 echo "Copying Swift ARM64 libraries"
@@ -76,14 +62,6 @@ do
 		"${KOTLIN_TARGET_JNI_ARM64}/"
 done
 
-echo "Copying Swift x86_64 libraries"
-for swift_sdk_lib in "${SWIFT_SDK_LIBS[@]}"
-do
-   cp -f \
-		"${SWIFT_ANDROID_SDK_X86_64}/${swift_sdk_lib}" \
-		"${KOTLIN_TARGET_JNI_X86_64}/"
-done
-
 # NDK Libs
 echo "Copying NDK ARM64 libraries"
 for swift_ndk_lib in "${SWIFT_NDK_LIBS[@]}"
@@ -91,14 +69,6 @@ do
    cp -f \
 		"${SWIFT_ANDROID_NDK_ARM64}/${swift_ndk_lib}" \
 		"${KOTLIN_TARGET_JNI_ARM64}/"
-done
-
-echo "Copying NDK x86_64 libraries"
-for swift_ndk_lib in "${SWIFT_NDK_LIBS[@]}"
-do
-   cp -f \
-		"${SWIFT_ANDROID_NDK_X86_64}/${swift_ndk_lib}" \
-		"${KOTLIN_TARGET_JNI_X86_64}/"
 done
 
 echo "All Done - Open Bindings/kotlin/RoyalVNCAndroidTest in Android Studio now"
